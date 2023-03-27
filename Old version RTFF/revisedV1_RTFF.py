4e08ce7245c448ea052751152b8b05119dedd788
monsterseppe <99758377+monsterseppe@users.noreply.github.com>
2023-03-27 16:41:19 +0200

    fixed indents

--- Old version RTFF/revisedV1_RTFF.py
+++ Old version RTFF/revisedV1_RTFF.py
@@ -105,96 +105,97 @@ while(cap.isOpened()):
             found_top_right = True
             break  
 
-if found_top_right == False:
-    for y in range(240):
-        _Line = line1[y]
-        if _Line > 0:
-            y1 = y
-            found_top_left = True
-            break
+    if found_top_right == False:
+        for y in range(240):
+            _Line = line1[y]
+            if _Line > 0:
+                y1 = y
+                found_top_left = True
+                break
             
-for y in range(240):
-    _Line = line1[239-y]
+    for y in range(240):
+        _Line = line1[239-y]
     if _Line > 0:
         y4 = y
         found_bottom_left = True
         break
         
-if found_bottom_left == False:
-    for y in range(240):
-        _Line = line2[239-y]
-        if _Line > 0:
-            y3 = y
-            found_bottom_right = True
-            break
+    if found_bottom_left == False:
+        for y in range(240):
+            _Line = line2[239-y]
+            if _Line > 0:
+                y3 = y
+                found_bottom_right = True
+                break
 
-if found_bottom_left == True:
-    L = y4
-    Line1 = 20
-elif found_bottom_right == True:
-    L = y3
-    Line1 = 120
+    if found_bottom_left == True:
+        L = y4
+        Line1 = 20
+    elif found_bottom_right == True:
+        L = y3
+        Line1 = 120
     
-if found_top_right == True:
-    R = y2
-    Line2 = 300
-elif found_top_left == True:
-    R = y1
-    Line2 = 200
+    if found_top_right == True:
+        R = y2
+        Line2 = 300
+    elif found_top_left == True:
+        R = y1
+        Line2 = 200
 
-diff = R - L
-tang = abs(diff) / 100
+    diff = R - L
+    tang = abs(diff) / 100
 
-if R > L:
-    angle = (math.atan(tang) * 180 / math.pi)
-else:
-    angle = 0 - (math.atan(tang) * 180 / math.pi)
+    if R > L:
+        angle = (math.atan(tang) * 180 / math.pi)
+    else:
+        angle = 0 - (math.atan(tang) * 180 / math.pi)
 
-font = cv2.FONT_HERSHEY_SIMPLEX
+    font = cv2.FONT_HERSHEY_SIMPLEX
 
-angle = round(angle, 5)
-text = str(angle)
+    angle = round(angle, 5)
+    text = str(angle)
 
-# Draw lines and angle text on the GPU
-mask_gpu = cvc.line(mask_gpu, (20,0), (20,240), (255,255,255), 2)
-mask_gpu = cvc.line(mask_gpu, (120,0), (120,240), (255,255,255), 2)
-mask_gpu = cvc.line(mask_gpu, (200,0), (200,240), (255,255,255), 2)
-mask_gpu = cvc.line(mask_gpu, (300,0), (300,240), (255,255,255), 2)
+    # Draw lines and angle text on the GPU
+    mask_gpu = cvc.line(mask_gpu, (20,0), (20,240), (255,255,255), 2)
+    mask_gpu = cvc.line(mask_gpu, (120,0), (120,240), (255,255,255), 2)
+    mask_gpu = cvc.line(mask_gpu, (200,0), (200,240), (255,255,255), 2)
+    mask_gpu = cvc.line(mask_gpu, (300,0), (300,240), (255,255,255), 2)
 
-frame_gpu = cvc.line(frame_gpu, (20,0), (20,240), (255,255,255), 2)
-frame_gpu = cvc.line(frame_gpu, (120,0), (120,240), (255,255,255), 2)
-frame_gpu = cvc.line(frame_gpu, (200,0), (200,240), (255,255,255), 2)
-frame_gpu = cvc.line(frame_gpu, (300,0), (300,240), (255,255,255), 2)
+    frame_gpu = cvc.line(frame_gpu, (20,0), (20,240), (255,255,255), 2)
+    frame_gpu = cvc.line(frame_gpu, (120,0), (120,240), (255,255,255), 2)
+    frame_gpu = cvc.line(frame_gpu, (200,0), (200,240), (255,255,255), 2)
+    frame_gpu = cvc.line(frame_gpu, (300,0), (300,240), (255,255,255), 2)
 
-frame_gpu = cvc.line(frame_gpu, (Line1, 239-L), (Line2, 239-R), (255,0,0), 3)
+    frame_gpu = cvc.line(frame_gpu, (Line1, 239-L), (Line2, 239-R), (255,0,0), 3)
 
-text_gpu = cvc.putText(mask_gpu, text, (20,210), font, 1, (255,255,255),2,cv2.LINE_4)
+    text_gpu = cvc.putText(mask_gpu, text, (20,210), font, 1, (255,255,255),2,cv2.LINE_4)
 
-test_gpu = cvc.cvtColor(mask_gpu, cv2.COLOR_GRAY2BGR)
+    test_gpu = cvc.cvtColor(mask_gpu, cv2.COLOR_GRAY2BGR)
 
-test_gpu = cvc.line(test_gpu, (Line1, 239-L), (Line2, 239-R), (255,0,0), 3)
+    test_gpu = cvc.line(test_gpu, (Line1, 239-L), (Line2, 239-R), (255,0,0), 3)
 
-# Concatenate the frames on the GPU
-frame_gpu[:, 320:, :] = test_gpu
+    # Concatenate the frames on the GPU
+    frame_gpu[:, 320:, :] = test_gpu
 
-# Display the frame on the screen
-cv2.imshow('both', frame_gpu)
+    # Display the frame on the screen
+    cv2.imshow('both', frame_gpu)
 
-toc = time.perf_counter()
+    toc = time.perf_counter()
 
-# Calculate the new motor speed with PID
-v = angle
-MV = pid(v)
+    # Calculate the new motor speed with PID
+    v = angle
+    MV = pid(v)
 
-# Clip the motor speed to the range [1, 179]
-MV = np.clip(MV, 1, 179)
+    # Clip the motor speed to the range [1, 179]
+    MV = np.clip(MV, 1, 179)
 
-# Set the servo position and motor speed
-kit.servo[8].angle = MV
-kit.continuous_servo[9].throttle = (abs(MV-90) / 90) / 3
+    # Set the servo position and motor speed
+    kit.servo[8].angle = MV
+    kit.continuous_servo[9].throttle = (abs(MV-90) / 90) / 3
         
     # Check if the user has pressed the 'q' key to exit the program
-if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 # Release the resources and close the windows
 cap.release()
 cv2.destroyAllWindows()
